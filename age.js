f5c65c73a772b24493d765c6554d3ce637b10edc
bryanhmiller <bryankalos@hotmail.com>
2017-01-23 20:41:44 -0600

    Added script tag to index.html and created added all javaScript to age.js

---
+++ age.js
@@ -0,0 +1,19 @@
+//1. How many hours are in a year
+//2. How many minutes are in a decade
+//3. How many seconds old they are
+//4. If they are older than some arbitrary number, console "I'm old", else "I'm young"
+
+var hoursInAYear = 24 * 365
+var minutesInADecade = 60 * hoursinayear * 10
+var howOld = 10
+var secondsOld = 60 * 60 * 24 *365 * howold
+
+console.log(hoursinayear);
+console.log(minutesInADecade);
+console.log(secondsOld);
+if (howOld > 10) {
+	console.log("I'm old");
+} else {
+	console.log("I'm young");
+}
+

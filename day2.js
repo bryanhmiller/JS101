e2be38cac4e1d17b37310d1e61ebf7aa1fc1f679
bryanhmiller <bryankalos@hotmail.com>
2017-01-24 19:22:37 -0600

    changed script reference for index.html, and added switch statement and if else to day2.js

---
+++ day2.js
@@ -0,0 +1,19 @@
+var day = "Friday";
+
+switch(day) {
+	case "Monday":
+		console.log("Boooooo");
+	case "Friday":
+		console.log("PARTAY");
+		break;
+	default:
+		console.log("not Monday");
+}
+
+if(day === "Monday"){
+	console.log("Boooooo");
+} else if(day === "Friday"){
+	console.log("PARTAY");
+} else {
+	console.log("not Monday");
+}

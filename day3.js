93d7c016b1a760a01016f1f775023efbe7ff5029
bryanhmiller <bryankalos@hotmail.com>
2017-01-28 12:17:44 -0600

    Added code to day3.js, missed most of it, computer was unplugged

--- day3.js
+++ day3.js
@@ -1,18 +1,45 @@
 // Objects
 
-var myCat = {
-	tail: "long",
-	age: 29,
-	color: "blue",
-	friends: ["zoe", "callan", "lauren"]
-}
-console.log("myCat before", myCat);
-myCat.owner = "zoe"
-console.log("myCat after", myCat);
-console.log("Age of cat: ", myCat.age);
-console.log("Color of cat: ", myCat["color"]);
+// var myCat = {
+// 	tail: "long",
+// 	age: 29,
+// 	color: "blue",
+// 	friends: ["zoe", "callan", "lauren"]
+// }
+// console.log("myCat before", myCat);
+// myCat.owner = "zoe"
+// console.log("myCat after", myCat);
+// console.log("Age of cat: ", myCat.age);
+// console.log("Color of cat: ", myCat["color"]);
 
-for(var myKey in myCat){
-	console.log("myKey", myKey);
-	console.log("values", myCat[myKey]);
+// for(var myKey in myCat){
+// 	console.log("myKey", myKey);
+// 	console.log("values", myCat[myKey]);
+// }
+
+// Functions
+
+function nuggetMaker(animal) {
+	var myNugget = "breading " + animal + " breading";
+	return myNugget;
 }
+
+function shackMaker(divLocation, typeOfNugget){
+	divLocation.innerHTML += typeOfNugget;
+}
+
+var chickenNuggets = nuggetMaker("chicken");
+shackMaker(availElement, chickenNuggets);
+var lobsterNuggets = nuggetMaker("lobster");
+shackMaker(availElement, lobsterNuggets);
+var tablets = nuggetMaker("myComputer");
+shackMaker(availElement, tablets);
+
+// var myanimals = ["cats", "pigs", "chickens", "cows", "bears"];
+
+// function myLooper(myArray) {
+// 	for (var i = 0; i < myArray.length; i++)
+// 	console.log(myArray[i]);
+// }
+
+// myLooper(myanimals);
